9c432b72fe2026a1317e28c1f584fce8ef46a913
Sridhar Ratnakumar <srid@srid.ca>
2025-09-14 15:38:25 -0400

    Apparently, optics need not be specified?

--- nix/modules/flake/hint.nix
+++ nix/modules/flake/hint.nix
@@ -4,8 +4,6 @@
       # Create a GHC environment with the packages we need
       hintGhc = config.haskellProjects.default.outputs.finalPackages.ghcWithPackages (ps: with ps; [
         hint-demo-types
-        optics-core
-        optics-th
       ]);
       # Environment variables required for `hint` to work correctly with our config.hs
       hintAttrs = rec {
